f31712a99bea6d08069d73fcfd650b7a185d2f30
kanisha99 <shahkanisha99@gmail.com>
2023-12-15 00:00:10 -0500

    Final Edits

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,94 +1,29 @@
+// Your data as a constant array
+const data = [
+    { County: "Bronx", Year: 2019, TotalCrime: 100 },
+    { County: "New York", Year: 2019, TotalCrime: 150 },
+    { County: "Queens", Year: 2019, TotalCrime: 120 },
+    { County: "Kings", Year: 2019, TotalCrime: 130 },
+    { County: "Richmond", Year: 2019, TotalCrime: 140 }
+];
 
-d3.json("docs/data_files/crimesnyc_nonnyc.json").then(function(data) {
-  const chart = createBubbleChart(data);
-  document.body.appendChild(chart);
+// Your D3 code for creating the graph
+data.forEach(function(d) {
+    d.TotalCrime = +d.TotalCrime;
 });
 
-function createBubbleChart(data) {
+var width = 500;
+var height = 300;
 
-  const width = 928;
-  const height = width;
-
-  // Create the color scale.
-  const color = d3.scaleLinear()
-    .domain([0, 5])
-    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-    .interpolate(d3.interpolateHcl);
-
-  // Compute the layout.
-  const pack = data => d3.pack()
-    .size([width, height])
-    .padding(3)
-    (d3.hierarchy(data)
-      .sum(d => d.value)
-      .sort((a, b) => b.value - a.value));
-  const root = pack(data);
-
-  // Create the SVG container.
-  const svg = d3.create("svg")
-    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height)
-    .attr("style", `max-width: 100%; height: auto; display: block; margin: 0 -14px; background: ${color(0)}; cursor: pointer;`);
+    .attr("height", height);
 
-  // Append the nodes.
-  const node = svg.append("g")
-    .selectAll("circle")
-    .data(root.descendants().slice(1))
-    .join("circle")
-    .attr("fill", d => d.children ? color(d.depth) : "white")
-    .attr("pointer-events", d => !d.children ? "none" : null)
-    .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
-    .on("mouseout", function() { d3.select(this).attr("stroke", null); })
-    .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
-
-  // Append the text labels.
-  const label = svg.append("g")
-    .style("font", "10px sans-serif")
-    .attr("pointer-events", "none")
-    .attr("text-anchor", "middle")
-    .selectAll("text")
-    .data(root.descendants())
-    .join("text")
-    .style("fill-opacity", d => d.parent === root ? 1 : 0)
-    .style("display", d => d.parent === root ? "inline" : "none")
-    .text(d => d.data.name);
-
-  // Create the zoom behavior and zoom immediately into the initial focus node.
-  svg.on("click", (event) => zoom(event, root));
-  let focus = root;
-  let view;
-  zoomTo([focus.x, focus.y, focus.r * 2]);
-
-  function zoomTo(v) {
-    const k = width / v[2];
-
-    view = v;
-
-    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("r", d => d.r * k);
-  }
-
-  function zoom(event, d) {
-    const focus0 = focus;
-
-    focus = d;
-
-    const transition = svg.transition()
-      .duration(event.altKey ? 7500 : 750)
-      .tween("zoom", d => {
-        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-        return t => zoomTo(i(t));
-      });
-
-    label
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-      .transition(transition)
-      .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-      .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-      .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-  }
-
-  return svg.node();
-}
+svg.selectAll("rect")
+    .data(data)
+    .enter().append("rect")
+    .attr("x", function(d, i) { return i * (width / data.length); })
+    .attr("y", function(d) { return height - d.TotalCrime; })
+    .attr("width", width / data.length - 1)
+    .attr("height", function(d) { return d.TotalCrime; })
+    .style("fill", "steelblue");
